9e281a1de5c8787001044f4ad47c27ff8b842d85
Endrial <endrialmp@gmail.com>
2022-07-20 20:38:03 +0700

    create footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,17 @@ import Discover from "../components/Discover";
 import NowPlaying from "../components/NowPlaying";
 import Popular from "../components/Popular";
 import Layout from "./Layout";
+import Footer from "../components/Footer";
 
 function Home() {
-
   return (
     <Layout>
       <NowPlaying />
       <Carousel />
-      <Box sx={{ marginTop: '20px' }} />
+      <Box sx={{ marginTop: "20px" }} />
       <Discover />
       <Popular />
+      <Footer />
     </Layout>
   );
 }
